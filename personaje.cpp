fd162086cdd81944f92a67720c4b37db7d931eea
MartinHargous <mmhargous@miuandes.cl>
2023-11-14 20:09:38 -0300

    cambio clases

--- personaje.cpp
+++ personaje.cpp
@@ -3,18 +3,23 @@
 
 using namespace std;
 
-personaje::personaje(char codigo, int salud_base, int equipo, int pos_x, int pos_y) {
-    this->codigo = codigo;
-    this->salud_base = salud_base;
-    this->equipo = equipo;
-    this->pos_x = pos_x;
-    this->pos_y = pos_y;
+personaje::personaje(char codigo,string tipo,int salud_base, int fuerza_max, int alcance_max, string direccion_ataque, int desplazamiento_max, int curacion) {
+    codigo = codigo;
+    tipo = tipo;
+    salud_base = salud_base;
+    fuerza_max = fuerza_max;
+    alcance_max = alcance_max;
+    direccion_ataque = direccion_ataque;
+    desplazamiento_max = desplazamiento_max;
+    curacion = curacion;
 }
 
+personaje::~personaje(){}
+
 void personaje::desplazarse(int objetivo) {
-    // Implementación del desplazamiento
+    
 }
 
 void personaje::actuar(char objetivo, char accion) {
-    // Implementación de la acción
+    
 }
--- personaje.h
+++ personaje.h
@@ -1,17 +1,18 @@
-#ifndef PERSONAJE_H
-#define PERSONAJE_H
+using namespace std;
 
 class personaje {
 public:
     char codigo;
+    string tipo;
     int salud_base;
-    int equipo;
-    int pos_x;
-    int pos_y;
-
-    personaje(char codigo, int salud_base, int equipo, int pos_x, int pos_y);
+    int fuerza_max;
+    int alcance_max;
+    string direccion_ataque;
+    int desplazamiento_max;
+    int curacion;
+    personaje(char codigo,string tipo, int salud_base ,int fuerza_max, int alcance_max, string direccion_ataque, int desplazamiento_max, int curacion);
     void desplazarse(int objetivo);
     void actuar(char objetivo, char accion);
 };
 
-#endif
+

73426cd323fc62825e107da92c263d2464f9ca6c
MartinHargous <mmhargous@miuandes.cl>
2023-11-14 20:10:56 -0300

    cambio clases

--- personaje.h
+++ personaje.h
@@ -11,6 +11,7 @@ public:
     int desplazamiento_max;
     int curacion;
     personaje(char codigo,string tipo, int salud_base ,int fuerza_max, int alcance_max, string direccion_ataque, int desplazamiento_max, int curacion);
+    ~personaje();
     void desplazarse(int objetivo);
     void actuar(char objetivo, char accion);
 };

02a78a66d7dc5b098b5705946a0e1dec1c293dee
MartinHargous <mmhargous@miuandes.cl>
2023-11-14 19:36:42 -0300

    test

--- personaje.h
+++ personaje.h
@@ -7,6 +7,8 @@ class personaje{
     int equipo;
     int pos_x;
     int pos_y;
+    
+    personaje(char, int, int, int, int);
 
     void dezplazarse(int objetivo);
     void actuar(char objetivo, char accion);

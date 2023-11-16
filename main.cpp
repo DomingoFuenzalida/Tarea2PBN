410ea78f12223468e84355b687397af4c6a2ecde
MartinHargous <mmhargous@miuandes.cl>
2023-11-16 00:31:55 -0300

    improme tablero correctamente, ahora si

--- main.cpp
+++ main.cpp
@@ -287,12 +287,6 @@ void imprimir_mapa(const vector<Personaje>& personajes, int filas, char*** mapa)
     }
     cout << endl;}
     cout << "----------------------"<<endl;
-    for (int i = 0; i < filas; i++) {
-            for (int j = 0; j < 4; j++) {
-                cout << mapa[i][j][0] << " ";
-            }
-            cout << endl;
-        }
 }
 
 void liberar_memoria(char***& mapa, int filas) {
